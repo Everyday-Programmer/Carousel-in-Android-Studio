2af2ccdb3307bc666481f452639f7ae5d7f3ca4c
Everyday Programmer <79357167+Everyday-Programmer@users.noreply.github.com>
2023-12-02 20:45:52 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/sliderimageview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.sliderimageview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageCarousel carousel = findViewById(R.id.carousel);
+
+        carousel.registerLifecycle(getLifecycle());
+
+        List<CarouselItem> list = new ArrayList<>();
+        list.add(new CarouselItem("https://images.unsplash.com/photo-1682685794761-c8e7b2347702?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "A person standing on top of a cliff"));
+        list.add(new CarouselItem("https://images.unsplash.com/photo-1682685797795-5640f369a70a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "A woman in a hat and coat walking through a canyon"));
+        list.add(new CarouselItem("https://images.unsplash.com/photo-1700771266232-7a31af68eb31?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"));
+        list.add(new CarouselItem("https://images.unsplash.com/photo-1682687218147-9806132dc697?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"));
+        carousel.setData(list);
+    }
+}
